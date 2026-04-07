b79deb6232a0fced6c13f57c73945e36a81f55f2
MIkhsanB546 <mikhsanb546@gmail.com>
2026-04-07 12:33:43 +0700

    feat: Selesai modul minggu 1

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -4,19 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use App\Models\Produk;
 
 
 class BerandaController extends Controller
 {
     public function berandaBackend()
     {
-        if (!isset(Auth::user()->email)) {
-            return redirect(route('backend.login'));
-        }
-
         return view('backend.v_beranda.index', [
-            'judul' => 'Halaman Beranda'
+            'judul' => 'Beranda',
+            'sub' => 'Halaman Beranda'
+        ]);
+    }
+
+    public function index()
+    {
+        $produk = Produk::where('status', 1)->orderBy('updated_at', 'desc')->paginate(6);
+        return view('v_beranda.index', [
+            'judul' => 'Halan Beranda',
+            'produk' => $produk,
         ]);
     }
 }
---
+++ resources/views/v_beranda/index.blade.php
@@ -0,0 +1,8 @@
+@extends('v_layouts.app')
+@section('content')
+    <!-- template -->
+
+
+
+    <!-- end template-->
+@endsection
